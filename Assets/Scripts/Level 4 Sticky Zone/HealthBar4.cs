96481c6ba49bf752fcad0d7f9c3abeb50ebb67eb
Anwar <anwarfiverr0@gmail.com>
2025-05-12 16:28:02 +0500

    added pause

--- Assets/Scripts/Level 4 Sticky Zone/HealthBar4.cs
+++ Assets/Scripts/Level 4 Sticky Zone/HealthBar4.cs
@@ -7,6 +7,7 @@ public class HealthBar4 : MonoBehaviour
     public Slider healthSlider;
     public int maxHealth = 100;
     public int currentHealth;
+    public GameObject PausePanel;
 
     void Start()
     {
@@ -20,6 +21,23 @@ public class HealthBar4 : MonoBehaviour
         currentHealth = Mathf.Clamp((int)newHealth, 0, maxHealth);
         healthSlider.value = currentHealth;
     }
+
+    public void GoHome()
+    {
+        Time.timeScale = 1f; // Unpause the game if it's paused
+        SceneManager.LoadScene("MainMenu"); // Replace "MainMenu" with your scene name
+    }
+    public void Pause()
+    {
+        PausePanel.SetActive(true);  // Show pause menu
+        Time.timeScale = 0f;          // Freeze the game
+    }
+    public void Resume()
+    {
+        PausePanel.SetActive(false); // Hide pause menu
+        Time.timeScale = 1f;          // Resume the game
+    }
+
 
     public void ReplayGame()
     {
